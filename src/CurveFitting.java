8b224c0fcf1f5c78470f3469a64f997dd62ba09b
ywsoliman <ywsoliman@gmail.com>
2022-11-15 03:12:41 +0200

    Implemented Selection, Crossover and Mutation

--- src/CurveFitting.java
+++ src/CurveFitting.java
@@ -15,6 +15,34 @@ public class CurveFitting {
         this.points = points;
     }
 
+    public int getNumberOfPoints() {
+        return numberOfPoints;
+    }
+
+    public void setNumberOfPoints(int numberOfPoints) {
+        this.numberOfPoints = numberOfPoints;
+    }
+
+    public int getDegree() {
+        return degree;
+    }
+
+    public void setDegree(int degree) {
+        this.degree = degree;
+    }
+
+    public ArrayList<Point> getPoints() {
+        return points;
+    }
+
+    public void setPoints(ArrayList<Point> points) {
+        this.points = points;
+    }
+
+    public void clearPoints() {
+        points.clear();
+    }
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -27,6 +55,7 @@ public class CurveFitting {
 
             numberOfPoints = sc.nextInt();
             degree = sc.nextInt();
+
             for (int j = 0; j < numberOfPoints; j++) {
                 x = sc.nextInt();
                 y = sc.nextInt();
@@ -40,43 +69,50 @@ public class CurveFitting {
 
     }
 
+    private double calculateError(ArrayList<Double> chromosome) {
+
+        double error = 0.0;
+
+        for (int i = 0; i < numberOfPoints; i++) {
+
+            double fitness = 0.0;
+            for (int j = 0; j < chromosome.size(); j++) {
+                fitness += chromosome.get(j) * Math.pow(points.get(i).getX(), j);
+            }
+            fitness -= points.get(i).getY();
+            fitness = Math.pow(fitness, 2);
+
+            error += fitness;
+
+        }
+
+        return 1.0 / error;
+
+    }
+
     private ArrayList<Chromosome> InitializePopulation(int populationSize) {
 
         ArrayList<Chromosome> totalChromosomes = new ArrayList<>(populationSize);
 
         int chromosomeSize = degree + 1;
-
         int rangeMin = -10;
-
         int rangeMax = 10;
 
         for (int i = 0; i < populationSize; i++) {
-            ArrayList<Double> chromosome = new ArrayList<>(chromosomeSize);
 
+            ArrayList<Double> chromosome = new ArrayList<>(chromosomeSize);
             double random;
 
             for (int gene = 0; gene < chromosomeSize; gene++) {
                 random = rangeMin + (rangeMax - rangeMin) * rand.nextDouble();
                 chromosome.add(random);
             }
-            totalChromosomes.add(new Chromosome(chromosome, 0));
+
+            totalChromosomes.add(new Chromosome(chromosome, calculateError(chromosome)));
+
         }
+
         return totalChromosomes;
-    }
-
-    private double calculateFitness(ArrayList<Double> chromosome) {
-
-    }
-
-    private ArrayList<Integer> calculateCumulative(ArrayList<Chromosome> chromosomes) {
-
-        ArrayList<Integer> cumulativeFitness = new ArrayList<>();
-
-        cumulativeFitness.add(chromosomes.get(0).getFitnessValue());
-        for (int i = 1; i < chromosomes.size(); i++)
-            cumulativeFitness.add(cumulativeFitness.get(i - 1) + chromosomes.get(i).getFitnessValue());
-
-        return cumulativeFitness;
 
     }
 
@@ -84,21 +120,13 @@ public class CurveFitting {
     private ArrayList<Chromosome> Selection(ArrayList<Chromosome> chromosomes) {
 
         ArrayList<Chromosome> selectedChromosomes = new ArrayList<>();
-        ArrayList<Integer> cumulativeFitness = calculateCumulative(chromosomes);
 
-        double randomNumber;
-        int firstCumulate = cumulativeFitness.get(0);
-        int lastCumulate = cumulativeFitness.get(cumulativeFitness.size() - 1);
+        int randomNumber;
 
         while (selectedChromosomes.size() != 2) {
 
-            randomNumber = rand.nextInt(lastCumulate - firstCumulate) + firstCumulate;
-            for (int i = 0; i < cumulativeFitness.size() - 1; i++) {
-                if (cumulativeFitness.get(i) >= randomNumber) {
-                    selectedChromosomes.add(chromosomes.get(i));
-                    break;
-                }
-            }
+            randomNumber = rand.nextInt(chromosomes.size() + 1);
+            selectedChromosomes.add(chromosomes.get(randomNumber));
 
         }
 
@@ -117,85 +145,141 @@ public class CurveFitting {
         afterCrossover.add(selectedChromosomes.get(0));
         afterCrossover.add(selectedChromosomes.get(1));
 
+
         if (Rc <= Pc) {
 
-            int Xc = rand.nextInt(numberOfPoints - 1) + 1;
+            int firstPoint = rand.nextInt(degree) + 1;
+            int secondPoint = firstPoint;
 
-            for (int i = Xc; i < items.size(); i++) {
-                int temp = afterCrossover.get(0).geneAt(i);
-                afterCrossover.get(0).setGeneAt(i, afterCrossover.get(1).geneAt(i));
-                afterCrossover.get(1).setGeneAt(i, temp);
+            while (firstPoint == secondPoint) {
+                secondPoint = rand.nextInt(degree) + 1;
             }
+
+            if (firstPoint > secondPoint) {
+                int temp = firstPoint;
+                firstPoint = secondPoint;
+                secondPoint = temp;
+            }
+
+            Chromosome tempSelected = afterCrossover.get(0);
+
+            for (int i = firstPoint; i < secondPoint; i++) {
+                afterCrossover.get(0).setGeneAt(i, afterCrossover.get(1).getGeneAt(i));
+                afterCrossover.get(1).setGeneAt(i, tempSelected.getGeneAt(i));
+            }
+
         }
+
         return afterCrossover;
+
     }
 
-    private void Mutation(ArrayList<Chromosome> parents, ArrayList<Chromosome> totalChromosomes, double Pm) {
+    private double getLowerBound(Chromosome chromosome) {
+
+        double lower = Double.MAX_VALUE;
+        for (int i = 0; i < chromosome.getGenes().size(); i++) {
+            if (lower > chromosome.getGeneAt(i)) {
+                lower = chromosome.getGeneAt(i);
+            }
+        }
+        return lower;
+
+    }
+
+    private double getUpperBound(Chromosome chromosome) {
+
+        double upper = Double.MIN_VALUE;
+        for (int i = 0; i < chromosome.getGenes().size(); i++) {
+            if (upper < chromosome.getGeneAt(i)) {
+                upper = chromosome.getGeneAt(i);
+            }
+        }
+        return upper;
+
+    }
+
+    private void Mutation(ArrayList<Chromosome> totalChromosomes, double Pm, int currentGeneration, int maxGeneration, int dependencyFactor) {
 
         double Rm;
+        double lowerBound, upperBound, deltaLower, deltaUpper, y, delta;
+        double random;
 
-        for (int i = 0; i < totalChromosomes.size(); i++) {
+        for (Chromosome chromosome : totalChromosomes) {
 
-            for (int j = 0; j < totalChromosomes.get(i).getGenes().size(); j++) {
+            lowerBound = getLowerBound(chromosome);
+            upperBound = getUpperBound(chromosome);
 
-                Rm = rand.nextDouble() / 10;
+            Rm = rand.nextDouble() / 10;
+
+            for (int i = 0; i < chromosome.getGenes().size(); i++) {
 
                 if (Rm <= Pm) {
-                    if (totalChromosomes.get(i).geneAt(j) == 1)
-                        totalChromosomes.get(i).setGeneAt(j, 0);
-                    else
-                        totalChromosomes.get(i).setGeneAt(j, 1);
-                }
-            }
 
-            if (calculateWeight(totalChromosomes.get(i).getGenes()) <= knapsackWeight)
-                totalChromosomes.get(i).setFitnessValue(calculateFitness(totalChromosomes.get(i).getGenes()));
-            else {
-                totalChromosomes.set(i, parents.get(i));
+                    random = rand.nextDouble();
+                    deltaLower = chromosome.getGeneAt(i) - lowerBound;
+                    deltaUpper = upperBound - chromosome.getGeneAt(i);
+
+                    if (random <= 0.5)
+                        y = deltaLower;
+                    else
+                        y = deltaUpper;
+
+                    delta = y * (Math.pow(1 - random, Math.pow(1 - (double)currentGeneration/maxGeneration, dependencyFactor)));
+
+                    if (y == deltaLower)
+                        chromosome.setGeneAt(i, chromosome.getGeneAt(i) - delta);
+                    else if (y == deltaUpper)
+                        chromosome.setGeneAt(i, chromosome.getGeneAt(i) + delta);
+
+
+                }
+
             }
 
         }
+
     }
 
     private void Replacement(ArrayList<Chromosome> oldGeneration, ArrayList<Chromosome> newGeneration) {
-        oldGeneration = newGeneration;
+
     }
 
     public void solve() {
 
         int populationSize = 20;
         int maxGeneration = 10;
+        int dependencyFactor = 2;
         double Pc = 0.6;
-        double Pm = 0.05;
+        double Pm = 0.02;
+
         ArrayList<Chromosome> currentGeneration = InitializePopulation(populationSize);
 
         // INSIDE A FOR LOOP FOR MAX GENERATIONS
         for (int i = 0; i < maxGeneration; i++) {
+
             ArrayList<Chromosome> newGeneration = new ArrayList<>();
 
             for (int j = 0; j < populationSize / 2; j++) {
-
                 ArrayList<Chromosome> selectedParents = Selection(currentGeneration);
                 ArrayList<Chromosome> parentsCrossover = Crossover(selectedParents, Pc);
                 newGeneration.add(parentsCrossover.get(0));
                 newGeneration.add(parentsCrossover.get(1));
-
             }
 
-            Mutation(currentGeneration, newGeneration, Pm);
+            Mutation(newGeneration, Pm, i, maxGeneration, dependencyFactor);
             Replacement(currentGeneration, newGeneration);
 
         }
 
-        double bestFitness = Integer.MIN_VALUE;
-        for (Chromosome c : currentGeneration) {
-            if (bestFitness < c.getFitnessValue()) {
-                bestFitness = c.getFitnessValue();
-            }
-        }
-        System.out.println(bestFitness);
+//        double bestFitness = Integer.MIN_VALUE;
+//        for (Chromosome c : currentGeneration) {
+//            if (bestFitness < c.getFitnessValue()) {
+//                bestFitness = c.getFitnessValue();
+//            }
+//        }
+//        System.out.println(bestFitness);
 
-//        clearItems();
+        clearPoints();
 
     }
 
@@ -224,6 +308,15 @@ public class CurveFitting {
         public void setFitnessValue(double fitnessValue) {
             this.fitnessValue = fitnessValue;
         }
+
+        public double getGeneAt(int pos) {
+            return genes.get(pos);
+        }
+
+        public void setGeneAt(int pos, double value) {
+            genes.set(pos, value);
+        }
+
     }
 
     static class Point {
